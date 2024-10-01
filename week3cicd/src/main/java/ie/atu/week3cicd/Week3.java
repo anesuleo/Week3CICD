e174f73f0a1a105a67c843f39de191e0bb35fb5a
G00425238@atu.ie <g00425238@atu,ie>
2024-10-01 11:51:46 +0100

    Created Calculator Method

--- week3cicd/src/main/java/ie/atu/week3cicd/Week3.java
+++ week3cicd/src/main/java/ie/atu/week3cicd/Week3.java
@@ -1,8 +1,5 @@
 package ie.atu.week3cicd;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person")
@@ -22,4 +19,37 @@ public class Week3 {
         return "Hi " + name;
     }
 
+    @GetMapping("/detail")
+    public String details(@RequestParam int age, @RequestParam String name){
+        return "name: " + name + " age: " + age;
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String method)
+    {
+        int ans = 0;
+        switch(method){
+            case "add":
+                ans = num1 + num2;
+                break;
+            case "subtract":
+                ans = num1 - num2;
+                break;
+            case "multiply":
+                ans = num1*num2;
+                break;
+            case "divide":
+                if(num2 == 0){
+                    String error = "Error";
+                    return error;
+                }
+                ans = num1/num2;
+                break;
+            default:
+                String error1 = "Error";
+                return error1;
+        }
+
+        return "If you " + method + " " + num1 + " and " + num2 + " you get " + ans;
+    }
 }
